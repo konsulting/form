99f0e21ef9d156595ef4319cc5ac9bb96b49903b
Keoghan Litchfield <keoghan@klever.co.uk>
2020-10-01 10:43:13 +0100

    Add config file
    Add Laravel config file to allow some easier setup.

---
+++ config/form.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class_namespaces' => [
+        'Konsulting\\FormBuilder\\Elements\\',
+    ],
+    'templates' => __DIR__ . '/../partials/bootstrap3',
+    'decorators' => [],
+];
--- src/Laravel/FormBuilderServiceProvider.php
+++ src/Laravel/FormBuilderServiceProvider.php
@@ -10,17 +10,33 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 class FormBuilderServiceProvider extends BaseServiceProvider
 {
     /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../../config/form.php' => config_path('form.php'),
+        ]);
+    }
+
+    /**
      * Register the form builder.
      *
      * @return void
      */
     public function register()
     {
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/form.php', 'form'
+        );
+
         $this->app->singleton(BaseFormBuilder::class, function () {
             return (new FormBuilder(
-                new Engine(__DIR__ . '/../../partials/bootstrap3'),
-                new ClassResolver('Konsulting\\FormBuilder\\Elements\\')
-            ))->addDecorators(config('laravel-form-builder.decorators'));
+                new Engine(config('form.templates')),
+                new ClassResolver(config('form.class_namespaces'))
+            ))->addDecorators(config('form.decorators'));
         });
     }
 }
